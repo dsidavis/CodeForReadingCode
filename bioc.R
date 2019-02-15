bf8fb7ac7548b78f7a1ca38b26d80f947883ac13
Duncan Temple Lang <duncan@r-project.org>
2019-02-14 20:33:59 -0800

    to install bioconductor packages we needed

---
+++ bioc.R
@@ -0,0 +1,10 @@
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("graph", version = "3.8")
+
+
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("Rgraphviz", version = "3.8")
+
